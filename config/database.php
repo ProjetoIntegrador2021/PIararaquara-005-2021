d9d8c3643844b7c63a1b494830d1ef0d3a13bd3c
Projeto Integrador <you@example.com>
2021-11-27 01:41:09 -0300

    database

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -66,11 +66,11 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'ec2-3-230-199-240.compute-1.amazonaws.com'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'd6gs4ggend914q'),
+            'username' => env('DB_USERNAME', 'bailulryfdrbxu'),
+            'password' => env('DB_PASSWORD', '7d1ed3cb1b435a963e5959a073b31d95b89d8af0f1c3743cc730670b48813828'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
